91a1eaca8d713f9912e2dbd8f107803c9eeb2756
Nano <swrdna@hotmail.com>
2025-06-17 18:48:44 +0800

    Update seed. Add sleep session and user follow dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-puts "Seeding data.."
+puts "✨ Seeding data"
+
+users = []
 
 %w[
   Liam
@@ -6,9 +8,40 @@ puts "Seeding data.."
   Noah
   Emma
   Lucas
+  Gavin
+  Maya
+  Sora
 ].each do |name|
-  User.create({ name: name })
+  user = User.new(name: name)
+
+  users << user if user.save
 end
 
+users.each do |user|
+  14.times do |i|
+    start_time = (i.days.ago + 22.hours + rand(0..120).minutes)
+    end_time = start_time + (rand(6..9).hours + rand(0..59).minutes)
 
-puts "✅ Done."
+    SleepSession.create(
+      user: user,
+      start_time: start_time,
+      end_time: end_time,
+      duration: (end_time - start_time).to_i,
+      created_at: start_time,
+      updated_at: start_time
+    )
+  end
+end
+
+users.each do |user|
+  users_to_follow = users.reject { |u| u == user }.sample(3)
+
+  users_to_follow.each do |user_to_follow|
+    UserFollow.create(
+      follower: user,
+      followed: user_to_follow
+    )
+  end
+end
+
+puts "✅ Done"
