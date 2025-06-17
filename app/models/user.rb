cb7cc4d2d5697eec5fab02bb5a793c1aabb4a351
Nano <swrdna@hotmail.com>
2025-06-17 10:03:07 +0800

    Lint files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   has_many :sleep_sessions, dependent: :destroy
 
   has_many :follows_given,
-           class_name: 'UserFollow',
+           class_name: "UserFollow",
            foreign_key: :follower_id,
            dependent: :destroy
   has_many :follows_received,
-           class_name: 'UserFollow',
+           class_name: "UserFollow",
            foreign_key: :followed_id,
            dependent: :destroy
 
--- app/models/user_follow.rb
+++ app/models/user_follow.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class UserFollow < ApplicationRecord
-  belongs_to :follower, class_name: 'User'
-  belongs_to :followed, class_name: 'User'
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 
   validates :follower_id, uniqueness: { scope: :followed_id, message: "You are already following this user" }
   validate :prevent_self_follow
