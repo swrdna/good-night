1181999b1ed80581c023cd405b18d5a0d876514f
Nano <swrdna@hotmail.com>
2025-06-17 10:03:17 +0800

    Lint file

--- db/migrate/20250616051335_create_sleep_sessions.rb
+++ db/migrate/20250616051335_create_sleep_sessions.rb
@@ -4,7 +4,7 @@ class CreateSleepSessions < ActiveRecord::Migration[7.2]
   def change
     create_table :sleep_sessions do |t|
       t.references :user, foreign_key: true, null: false
-      t.datetime :start_time, null: false, default: -> { 'CURRENT_TIMESTAMP' }
+      t.datetime :start_time, null: false, default: -> { "CURRENT_TIMESTAMP" }
       t.datetime :end_time
 
       t.timestamps null: false
